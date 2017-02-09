524460754be5f65cbd2d49c109b6984fac6208f4
Dylan Stackhouse and Sarah Leahy <sarah@>
2017-02-09 08:55:38 -0800

    refactor dealership app into twig

--- app/app.php
+++ app/app.php
@@ -4,34 +4,19 @@
     require_once __DIR__."/../src/Car.php";
 
     $app = new Silex\Application();
+    $app["debug"] = true;
 
-    $app->get("/", function() {
-      return
-      "<!DOCTYPE html>
-        <html>
-        <head>
-        <body>
-            <div class='container'>
-                <h1>Find a Car!</h1>
-                <form action='/Car'>
-                    <div class='form-group'>
-                        <label for='price'>Enter Maximum Price:</label>
-                        <input id='price' name='price' class='form-control' type='number'>
-                    </div>
-                    <div class='form-group'>
-                        <label for='miles'>Enter Maximum mileage:</label>
-                        <input id='miles' name='miles' class='form-control' type='number'>
-                    </div>
-                    <button type='submit' class='btn-success'>Submit</button>
-                </form>
-             </div>
-         </body>
-        </html>";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render('input-form.html.twig');
     });
 
  // adfdasfds
 
-    $app->get("/Car", function() {
+    $app->get("/Car", function() use($app) {
           $porsche = new Car("2014 Porsche 911", 114991, 7864);
           $ford = new Car("2011 Ford F450", 55995, 14241);
           $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
@@ -39,29 +24,15 @@
           $mazda = new Car("2016 Mazda 6", 25000, 50000);
 
           $cars = array($ford, $lexus, $mercedes, $mazda, $porsche);
-          $output = "<h1>Your Car Dealership</h1>";
           $cars_matching_search = array();
+
           foreach ($cars as $car) {
-              if ($car->getPrice() < $_GET["price"] && $car->getMiles() < $_GET["miles"]) {
-                  array_push($cars_matching_search, $car);
+                  if ($car->getPrice() < $_GET["price"] && $car->getMiles() < $_GET["miles"]) {
+                        array_push($cars_matching_search, $car);
+                  }
             }
-        }
-            if ($cars_matching_search == null){
-                return "Sorry no cars match your search.";
-            }   else{
-                    foreach ($cars_matching_search as $car) {
-                    $our_price = $car->getPrice();
-                    $our_model = $car->getModel();
-                    $our_miles = $car->getMiles();
-                    $output = $output .
-                     "<li> $our_model </li>
-                     <ul>
-                     <li> $$our_price </li>
-                     <li> Miles: $our_miles </li>
-                     </ul>";
-                }
-            }
-        return $output;
+
+        return $app['twig']->render('Car.html.twig', array("cars" => $cars_matching_search));
     });
 
     return $app;
